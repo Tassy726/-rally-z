b9ff2e62a3e5fc2f645a50aac979861c8d39dc42
Tassy009 <91830909+Tassy726@users.noreply.github.com>
2023-11-29 23:28:17 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,155 @@
+namespace SpriteKind {
+    export const Map = SpriteKind.create()
+}
+let myMinimap: minimap.Minimap = null
+let mySprite = sprites.create(img`
+    . . . . . . e e c c e e . . . . 
+    . . . . . e 2 2 2 2 2 2 e . . . 
+    . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+    . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+    . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+    . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+    . . . e 2 c e c c c c e c 2 e . 
+    . . . e 2 e c b b b b c e 2 e . 
+    . . . e 2 e b b b b b b e 2 e . 
+    . . . e e e e e e e e e e e e . 
+    . . . f e d e e e e e e d e f . 
+    . . . f e 2 d e e e e d 2 e f . 
+    . . . f f e e e e e e e e f f . 
+    . . . . f f . . . . . . f f . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`レベル1`)
+scene.cameraFollowSprite(mySprite)
+let MapSprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Map)
+forever(function () {
+    if (controller.up.isPressed()) {
+        mySprite.setImage(img`
+            . . . . . . e e c c e e . . . . 
+            . . . . . e 2 2 2 2 2 2 e . . . 
+            . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+            . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+            . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+            . . . e 2 c e c c c c e c 2 e . 
+            . . . e 2 e c b b b b c e 2 e . 
+            . . . e 2 e b b b b b b e 2 e . 
+            . . . e e e e e e e e e e e e . 
+            . . . f e d e e e e e e d e f . 
+            . . . f e 2 d e e e e d 2 e f . 
+            . . . f f e e e e e e e e f f . 
+            . . . . f f . . . . . . f f . . 
+            `)
+    }
+    if (controller.down.isPressed()) {
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 2 2 2 2 2 2 . . . . 
+            . . . . . 2 2 4 4 2 2 2 2 . . . 
+            . . . . . c 4 2 2 2 2 2 c . . . 
+            . . . . 2 c 4 2 2 2 2 2 c 2 . . 
+            . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+            . . . f 2 c 4 2 2 2 2 2 c 2 f . 
+            . . . f e c 2 2 2 2 2 2 c e f . 
+            . . . f 2 c 2 b b b b 2 c 2 f . 
+            . . . e 2 2 b c c c c b 2 2 e . 
+            . . . e e b c c c c c c b e e . 
+            . . . f e 4 4 4 4 4 4 4 4 e f . 
+            . . . f e d 2 2 2 2 2 2 d e f . 
+            . . . . 2 d d 2 2 2 2 d d 2 f . 
+            . . . . f 2 d 2 2 2 2 d 2 f . . 
+            . . . . . e 2 2 2 2 2 2 e . . . 
+            `)
+    }
+    if (controller.left.isPressed()) {
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 2 2 2 2 2 2 2 2 . . 
+            . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+            . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+            . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+            . d 2 2 4 c b e e e e e e e 2 c 
+            . 2 2 2 4 b e e b b b e b b e 2 
+            . 2 2 2 2 2 e b b b b e b b b e 
+            . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+            . 2 d d 2 e f e e e f e e e e e 
+            . d d 2 e e e f e e f e e e e e 
+            . e e e e e e e f f f e e e e e 
+            . e e e e f f f e e e e f f f f 
+            . . . e f f f f f e e f f f f f 
+            . . . . f f f f . . . . f f f . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+    if (controller.right.isPressed()) {
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+            . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+            . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+            . 2 c 2 e e e e e e e b c 4 2 2 
+            . 2 2 e b b e b b b e e b 4 2 2 
+            . 2 e b b b e b b b b e 2 2 2 2 
+            . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+            . e e e e e e f e e e f e 2 d d 
+            . e e e e e e f e e f e e e 2 d 
+            . e e e e e e f f f e e e e e e 
+            . e f f f f e e e e f f f e e e 
+            . . f f f f f e e f f f f f e . 
+            . . . f f f . . . . f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+})
+forever(function () {
+    myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
+    minimap.includeSprite(myMinimap, mySprite)
+    if (mySprite.overlapsWith(MapSprite)) {
+        MapSprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else {
+        MapSprite.setImage(minimap.getImage(myMinimap))
+    }
+    MapSprite.setPosition(scene.cameraProperty(CameraProperty.X) + 60, scene.cameraProperty(CameraProperty.Y))
+})
+game.onUpdateInterval(100, function () {
+	
+})
